a34d64899875e8cbcefd65ad815b1e7598482484
HinataYasukawa <h1i2n2a62208@gmail.com>
2023-07-11 17:10:20 +0900

    add history

--- count.c
+++ count.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+
+#define MAXNUM 10
 
 int main(void){
-    printf("please input number 1~9.\n\n");
+    srand(time(NULL));
+    printf("please input number 0~999.\n\n");
 
-    int ans = (rand() % 9) + 1;
+    int ans = rand() % 1000 + 1;
     int no;
-    bool cor = false;
+    int num[MAXNUM];
+    int stage = 0;
 
-    while(cor == false){
+    while(no != ans && stage < MAXNUM){
         printf("your answer:");
         scanf("%d", &no);
+        num[stage] = no;
+        stage++;
+
+        if(no > ans) printf("It's bigger.\n\nlast %d times.", MAXNUM - stage);
+        else if(no < ans) printf("It's smaller.\n\nlast %d times.", MAXNUM - stage);
+    }
 
         if(no == ans){
             printf("correct.\n");
-            printf("ans = %d", ans);
-            cor = true;
+        } else{
+        printf("you are loser.gg.");
+    }
+    printf("ans = %d\n", ans);
+    printf("It took %d times. The answer history is...\n-------------------------------------------\n", stage);
+    for(int i = 0; i < stage; i++){
+        if(i+1 < 10){
+            printf("%d  : %-4d\n",i+1, num[i]);
+        }else{
+            printf("%d : %-4d\n",i+1, num[i]);
         }
-        else if(no > ans) printf("It's bigger.\n\n");
-        else if(no < ans) printf("It's smaller.\n\n");
     }
 
     return 0;
